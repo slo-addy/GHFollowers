3b67ba5fbc40d21d95ec64781aef93f395a77f3e
slo-addy <addison.francisco@gmail.com>
2020-04-29 18:57:47 -0700

    Swift Result

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -17,14 +17,14 @@ class FollowerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
 
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
+            case .success(let followers):
+                print("Followers count = \(followers.count)")
+                print(followers)
             }
-
-            print("Followers count = \(followers.count)")
-            print(followers)
         }
     }
 
